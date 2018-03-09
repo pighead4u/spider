3b995f3ff24266d743e865f1868c3acc69178ca8
serendipity4u <zhuhuanhuan@qccr.com>
2018-03-09 13:38:32 +0800

    add:jobs文件夹

---
+++ jobs/main.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from spider_lagou import Crawl
+
+cities = ["%E5%B9%BF%E5%B7%9E", "%E6%9D%AD%E5%B7%9E", "%E4%B8%8A%E6%B5%B7", "%E5%8C%97%E4%BA%AC", "%E6%B7%B1%E5%9C%B3"]
+filenames = ["guangzhou", 'hangzhou', 'shanghai', 'beijing', 'shenzhen']
+
+for (city, filename) in zip(cities, filenames):
+    print("{city}--{filename}".format(city=city, filename=filename))
+    spider = Crawl(city, filename)
+    spider.getjobdata(50)
---
+++ jobs/spider_lagou.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import time
+
+import requests
+import xlwt
+
+
+class Crawl:
+
+    def __init__(self, city, filename):
+        self.city = city
+        self.filename = filename
+        self.excel = xlwt.Workbook()
+        self.sheet = self.excel.add_sheet('lagou', cell_overwrite_ok=True)
+        self.line = 1;
+
+    def getjobdata(self, pagesize):
+
+        for pn in [x + 1 for x in range(pagesize)]:
+            data = self._build_request_data(pn)
+            response = self._get_http_data(self.city, data)
+            is_stop = self._write_data_2file(response)
+            time.sleep(2)
+
+            if is_stop:
+                break
+
+        self.excel.save('{fileName}.xlsx'.format(fileName=self.filename))
+
+    def _build_request_data(self, pagenumb):
+        if pagenumb == 1:
+            is_first = 'true'
+        else:
+            is_first = 'false'
+
+        data = {
+            'first': is_first,
+            'pn': str(pagenumb),
+            'kd': 'Android',
+        }
+        return data
+
+    def _get_http_data(self, city, data):
+        HEADERS = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36',
+            'Referer': 'https://www.lagou.com/jobs/list_Android?px=default&city={city}'.format(city=city),
+            'Origin': 'https://www.lagou.com',
+            'Host': 'www.lagou.com',
+            'Cookie': 'user_trace_token=20171018150757-11d7072b-b3d3-11e7-9595-5254005c3644; LGUID=20171018150757-11d70ccb-b3d3-11e7-9595-5254005c3644; Hm_lvt_9d483e9e48ba1faa0dfceaf6333de846=1510029000,1510044572,1510069860,1510103314; showExpriedIndex=1; showExpriedCompanyHome=1; showExpriedMyPublish=1; hasDeliver=5; index_location_city=%E5%85%A8%E5%9B%BD; JSESSIONID=ABAAABAACBHABBIE7CD5943FF544D0FEE9479A3B6501DD3; TG-TRACK-CODE=index_navigation; _gat=1; _gid=GA1.2.273848703.1512557694; _ga=GA1.2.589815693.1508310478; Hm_lvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1512206891,1512220618,1512557694,1512610017; Hm_lpvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1512616430; LGSID=20171207101355-472ffbae-daf4-11e7-9c4b-5254005c3644; LGRID=20171207111349-a54c84d8-dafc-11e7-8804-525400f775ce; SEARCH_ID=3f98f136bc1d4fb98886a9f6fdebfff1'
+        }
+
+        url = 'https://www.lagou.com/jobs/positionAjax.json?px=default&city={city}&needAddtionalResult=false&isSchoolJob=0'.format(
+            city=city)
+
+        response = requests.post(url, data=data, headers=HEADERS)
+
+        return response
+
+    def _write_data_2file(self, response):
+        result = response.json()
+        if result.get('content').get('pageNo') == 0:
+            return True
+        jobs = result.get('content').get('positionResult').get('result')
+        self.sheet.write(0, 0, 'positionName')
+        self.sheet.write(0, 1, 'salary')
+        self.sheet.write(0, 2, 'workYear')
+        self.sheet.write(0, 3, 'education')
+        self.sheet.write(0, 4, 'jobNature')
+        self.sheet.write(0, 5, 'city')
+        self.sheet.write(0, 6, 'companyShortName')
+        self.sheet.write(0, 7, 'district')
+        self.sheet.write(0, 8, 'positionLables')
+        self.sheet.write(0, 9, 'secondType')
+        self.sheet.write(0, 10, 'companyFullName')
+
+        for job in jobs:
+            self.sheet.write(self.line, 0, job['positionName'])
+            self.sheet.write(self.line, 1, job['salary'])
+            self.sheet.write(self.line, 2, job['workYear'])
+            self.sheet.write(self.line, 3, job['education'])
+            self.sheet.write(self.line, 4, job['jobNature'])
+            self.sheet.write(self.line, 5, job['city'])
+            self.sheet.write(self.line, 6, job['companyShortName'])
+            self.sheet.write(self.line, 7, job['district'])
+            self.sheet.write(self.line, 8, job['positionLables'])
+            self.sheet.write(self.line, 9, job['secondType'])
+            self.sheet.write(self.line, 10, job['companyFullName'])
+            self.line += 1
+
+        return False
